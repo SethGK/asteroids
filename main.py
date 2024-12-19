c7e2d1cf5d62ac270ed3def6d3b563160213f441
SethK99 <sgkloster@gmail.com>
2024-12-18 18:45:05 -0800

    Drew Player Sprite

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 pygame.init()
 
 from constants import *
+from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -13,6 +14,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,8 +24,10 @@ def main():
         black = (0,0,0)
         screen.fill(black)
 
+        
+        player.draw(screen)
+        
         dt = clock.tick(60)/1000
-
         pygame.display.flip()
 
 
